66cb9cf31205cba6c7b4e66fa7f3b34d30358e01
Fabio Amaral <fabio.amaral@efabrika.com.br>
2017-08-20 15:10:05 -0300

    Primeiro commit

---
+++ src/main/java/com/bluesky/okAll/OkAllApplication.java
@@ -0,0 +1,12 @@
+package com.bluesky.okAll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkAllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkAllApplication.class, args);
+	}
+}
